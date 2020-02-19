4556c1ab8f528da739e4e5303d5849b5de510009
EmiKusaba <eeemi615@gmail.com>
2020-02-18 20:10:08 -0600

    deployed

--- sql/connection.js
+++ sql/connection.js
@@ -7,9 +7,9 @@ class Connection {
 
       const config = {
         connectionLimit: 100,
-        host: 'localhost',
+        host: '34.68.3.134',
         user: 'root',
-        password: 'password',
+        password: 'Argo20180814',
         database: 'admin'
       }
 
